4405a7e155b117937b43d9dd0f03a35209c564f4
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2021-12-12 11:16:53 +0330

    added simple event bus

---
+++ event-bus/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { json } = require("body-parser");
+const { axios } = require("axios");
+// const cors = require("cors");
+
+const app = express();
+app.use(json());
+// app.use(cors());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  // post service
+  axios.post("http://localhost:4000/events", event);
+
+  // comment service
+  axios.post("http://localhost:4002/events", event);
+
+  // query service
+  axios.post("http://localhost:4003/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
